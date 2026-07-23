be852849e2a01e9848a3da3e691f3b3d6f00ce8c
said <asaidalibacari@gmail.com>
2026-07-23 11:00:37 +0000

    application bonjour

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+console.log("Bonjour mes chers compatriotes");
+app.listen(3000,() =>{
+console.log("application demarrée sur le port 3000");
+});
+
